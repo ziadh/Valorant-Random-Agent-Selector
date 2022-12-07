a13a76d9361eafc5b837863031fef25ff3819ee1
ziadh <zhussein1996@gmail.com>
2022-12-07 11:18:08 -0500

    1.10
    Patch Notes:
    -Added the option to choose when to run the script. This enables the user to open the program while in queue or before queue and still be able to run the script when the game is found
    -Added the option to reroll at the end of the pick

--- main.py
+++ main.py
@@ -1,95 +1,128 @@
+# Release #: 1.10
+# Patch Notes:
+# Added the option to choose when to run the script. This enables the user to open the program while in queue or before queue and still be able to run the script when the game is found
+# Added specific messages for each agent when picked
+# Added the option to reroll at the end of the pick
+
 import random
-import os
 import pyautogui as pe
 import time
 
-print("Welcome to the Random Agent Selector.")
-time.sleep(3)
-agents = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Fade", "Harbor", "Jett",
-          "KAY/O", "KillJoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"]
 
-number = random.randint(0, 19)
-print("You are playing as " + agents[number] + ". Enjoy!")
-
-if number == 0:  # Astra
-    pe.click(579, 926)
-    pe.click(989, 821)
-
-if number == 1:  # Breach
-    pe.click(666, 928)
-    pe.click(989, 821)
-
-if number == 2:  # Brim
-    pe.click(744, 916)
-    pe.click(989, 821)
-
-if number == 3:  # Chamber
-    pe.click(815, 922)
-    pe.click(989, 821)
-
-if number == 4:  # Cypher
-    pe.click(902, 923)
-    pe.click(989, 821)
-
-if number == 5:  # fade
-    pe.click(995, 923)
-    pe.click(989, 821)
+def invalid_input():
+    print("Invalid input. ")
+    print("Terminating application")
+    time.sleep(2)
+    quit()
 
 
-if number == 6:  # harbor
-    pe.click(1104, 911)
-    pe.click(989, 821)
+def random_agent():
 
-if number == 7:  # Jett
-    pe.click(1074, 926)
-    pe.click(989, 821)
+    time.sleep(3)
+    agents = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Fade", "Harbor", "Jett",
+              "KAY/O", "KillJoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"]
 
-if number == 8:  # kayo
-    pe.click(1168, 929)
-    pe.click(989, 821)
+    number = random.randint(0, 19)
+    print("You are playing as " + agents[number] + ". Enjoy!")
 
-if number == 9:  # kj
-    pe.click(1236, 928)
-    pe.click(989, 821)
+    if number == 0:  # Astra
+        pe.click(582, 921)
+        #pe.click(989, 821)
 
-if number == 10:  # neon
-    pe.click(1326, 924)
-    pe.click(989, 821)
+    if number == 1:  # Breach
+        pe.click(673, 935)
+        #pe.click(989, 821)
 
-if number == 11:  # omen
-    pe.click(582, 1002)
-    pe.click(989, 821)
+    if number == 2:  # Brim
+        pe.click(743, 922)
+        #pe.click(989, 821)
 
-if number == 12:  # phoenix
-    pe.click(678, 1005)
-    pe.click(989, 821)
+    if number == 3:  # Chamber
+        pe.click(834, 927)
+        #pe.click(989, 821)
 
-if number == 13:  # raze
-    pe.click(731, 1003)
-    pe.click(989, 821)
+    if number == 4:  # Cypher
+        pe.click(926, 915)
+        #pe.click(989, 821)
 
-if number == 14:  # reyna
-    pe.click(821, 1006)
-    pe.click(989, 821)
+    if number == 5:  # fade
+        pe.click(1006, 936)
+        #pe.click(989, 821)
 
-if number == 15:  # Sage
-    pe.click(908, 1005)
-    pe.click(989, 821)
+    if number == 6:  # harbor
+        pe.click(1097, 929)
+        #pe.click(989, 821)
 
-if number == 16:  # skye
-    pe.click(989, 1003)
-    pe.click(989, 821)
+    if number == 7:  # Jett
+        pe.click(1172, 929)
+        #pe.click(989, 821)
 
-if number == 17:  # sova
-    pe.click(1075, 1001)
-    pe.click(989, 821)
+    if number == 8:  # kayo
+        pe.click(1172, 929)
+        #pe.click(989, 821)
 
-if number == 18:  # viper
-    pe.click(1157, 1004)
-    pe.click(989, 821)
+    if number == 9:  # kj
+        pe.click(1332, 924)
+        #pe.click(989, 821)
 
-if number == 19:  # yoru
-    pe.click(1245, 1002)
-    pe.click(989, 821)
+    if number == 10:  # neon
+        pe.click(573, 1000)
+        #pe.click(989, 821)
 
-time.sleep(5)
+    if number == 11:  # omenn
+        pe.click(668, 1013)
+        #pe.click(989, 821)
+
+    if number == 12:  # phoenix
+        pe.click(759, 1011)
+        #pe.click(989, 821)
+
+    if number == 13:  # raze
+        pe.click(837, 1004)
+        #pe.click(989, 821)
+
+    if number == 14:  # reyna
+        pe.click(929, 1012)
+        #pe.click(989, 821)
+
+    if number == 15:  # Sage
+        pe.click(993, 1011)
+        #pe.click(989, 821)
+
+    if number == 16:  # skye
+        pe.click(993, 1011)
+        #pe.click(989, 821)
+
+    if number == 17:  # sova
+        pe.click(1164, 1008)
+        #pe.click(989, 821)
+
+    if number == 18:  # viper
+        pe.click(1256, 1004)
+        #pe.click(989, 821)
+
+    if number == 19:  # yoru
+        pe.click(1326, 1008)
+        #pe.click(989, 821)
+
+    reroll_choice = input("Would you like to reroll? y/n \n")
+    if reroll_choice == "y" or reroll_choice == "yes" or reroll_choice == "":
+        random_agent()
+    else:
+        print("Terminating application in 3 seconds.")
+        time.sleep(3)
+
+
+print("Welcome to the Random Agent Selector")
+time.sleep(2)
+
+firstinput = input("Would you like to proceed? (y/n) \n").lower()
+if firstinput == "y" or firstinput == "yes" or firstinput == "":
+    time.sleep(1)
+    random_agent()
+elif firstinput == "n" or firstinput == "no":
+    print("Terminating application...")
+    pe.sleep(2)
+    quit()
+else:
+    invalid_input()
